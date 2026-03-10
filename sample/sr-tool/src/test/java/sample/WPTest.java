81670fb68d52e3246d2a93e22a1adcc8ff1fc28d
baichen <baich@neusoft.com>
2026-03-10 12:36:40 +0900

    update

--- sample/sr-tool/src/test/java/sample/WPTest.java
+++ sample/sr-tool/src/test/java/sample/WPTest.java
@@ -12,7 +12,7 @@ import cbai.util.json.JSONUtil;
 public class WPTest {
 
     public static void main(String[] args) {
-        File cahceFile = new File("./wp_db.cache");
+        File cahceFile = new File("./target/wp_db.cache");
         String basePath = "D:\\WORK\\128SYIS25142_devora\\SVN_CITS\\documents\\04_内部設計\\02_作成ドキュメント\\50_機能設計\\80_DB設計書\\02_テーブル定義\\";
 //        String excelDir = "./db/第４．０版";
         ITableBeanReader reader = new WPTableBeanReader(basePath);

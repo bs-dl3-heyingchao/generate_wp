82e26d5ec36e5b18fb91833358cc649b42d619eb
baichen <baich@neusoft.com>
2026-03-12 19:53:01 +0900

    add wp templates

--- wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/WpCodeGenerator.java
+++ wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/WpCodeGenerator.java
@@ -1,15 +1,22 @@
 package com.neusoft.bsdl.wptool.generate;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.File;
 
+import com.neusoft.bsdl.wptool.core.context.WPContext;
 import com.neusoft.bsdl.wptool.core.model.ScreenExcelContent;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class WpCodeGenerator {
 
-    private static final Logger log = LoggerFactory.getLogger(WpCodeGenerator.class);
+    private WPContext context;
 
-    public void generate(ScreenExcelContent screenExcelContent) {
+    public WpCodeGenerator(WPContext context) {
+        this.context = context;
+    }
+
+    public void generate(ScreenExcelContent screenExcelContent, File outputDir) {
         log.info("Generation flow placeholder created. Implement concrete generation logic here.");
     }
 }
--- wp-tool-generate/src/test/java/com/neusoft/bsdl/wptool/generate/WpCodeGeneratorTest.java
+++ wp-tool-generate/src/test/java/com/neusoft/bsdl/wptool/generate/WpCodeGeneratorTest.java
@@ -1,20 +1,22 @@
 package com.neusoft.bsdl.wptool.generate;
 
+import java.io.File;
+
 import com.neusoft.bsdl.wptool.core.context.WPContext;
 import com.neusoft.bsdl.wptool.core.io.FileSource;
 import com.neusoft.bsdl.wptool.core.io.LocalFileSource;
 import com.neusoft.bsdl.wptool.core.model.ScreenExcelContent;
 import com.neusoft.bsdl.wptool.core.service.ParseExcelUtils;
-import com.neusoft.bsdl.wptool.validator.WPScreenValidator;
 
 public class WpCodeGeneratorTest {
 
     public static void main(String[] args) throws Exception {
+        File outputDir = new File("target/output");
         FileSource source = new LocalFileSource(args[0]);
         WPContext context = WPContext.create();
-        WPScreenValidator screenChecker = new WPScreenValidator(context);
+        WpCodeGenerator codeGenerator = new WpCodeGenerator(context);
         ScreenExcelContent screenExcelContent = ParseExcelUtils.parseScreenExcel(source);
-        screenChecker.validateParseContent(screenExcelContent);
+        codeGenerator.generate(screenExcelContent, outputDir);
     }
 
 }

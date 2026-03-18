641c1c0414a8c38dad224cbe8c24be1ba5b532b3
heyingchao@neusoft.com <heyingchao@neusoft.com>
2026-03-18 18:49:25 +0900

    no message

--- wp-tool-core/src/main/java/com/neusoft/bsdl/wptool/core/model/ProcessingFuncSpecification.java
+++ wp-tool-core/src/main/java/com/neusoft/bsdl/wptool/core/model/ProcessingFuncSpecification.java
@@ -1,11 +1,11 @@
 package com.neusoft.bsdl.wptool.core.model;
 
-import java.util.Map;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class ProcessingFuncSpecification {
-	/**1.パラメータ⇒Key：順、value:項目*/
-	private Map<String,String> params;
+	/**1.パラメータ*/
+	private List<ProcessingFuncSpecificationParam> params;
 }
---
+++ wp-tool-core/src/main/java/com/neusoft/bsdl/wptool/core/model/ProcessingFuncSpecificationParam.java
@@ -0,0 +1,11 @@
+package com.neusoft.bsdl.wptool.core.model;
+
+import lombok.Data;
+
+@Data
+public class ProcessingFuncSpecificationParam {
+	//順
+	private String sort;
+	//項目
+	private String logicName;
+}

2c4ab51d6aff6ec383035a674b822828edc984cc
baichen <baich@neusoft.com>
2026-03-19 15:52:18 +0900

    追加API

---
+++ wp-tool-api/src/main/java/com/neusoft/bsdl/wptool/api/dto/GeneratedCodeZipResponse.java
@@ -0,0 +1,6 @@
+package com.neusoft.bsdl.wptool.api.dto;
+
+import java.util.List;
+
+public record GeneratedCodeZipResponse(String zipBase64, String taskId, List<String> errorLog, List<String> warnLog) {
+}

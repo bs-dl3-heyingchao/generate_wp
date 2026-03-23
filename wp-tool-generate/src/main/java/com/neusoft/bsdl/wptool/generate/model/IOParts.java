7330b5f4b355fa2c06a7f9488ea5e4f00dc3e563
baichen <baich@neusoft.com>
2026-03-23 19:12:44 +0900

    Merge remote-tracking branch 'origin/main' into feature/wp-tool-generate

---
+++ wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/model/IOParts.java
@@ -0,0 +1,11 @@
+package com.neusoft.bsdl.wptool.generate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class IOParts {
+    public String parts;
+    public String position;
+}

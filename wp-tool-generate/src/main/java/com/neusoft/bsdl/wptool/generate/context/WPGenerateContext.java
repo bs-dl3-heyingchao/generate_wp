0aa5a1bd2833a6ba801f427117cbb1e6ed6a6dc9
baichen <baich@neusoft.com>
2026-03-24 16:28:03 +0900

    update sqlconvert

--- wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/context/WPGenerateContext.java
+++ wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/context/WPGenerateContext.java
@@ -2,14 +2,16 @@ package com.neusoft.bsdl.wptool.generate.context;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.neusoft.bsdl.wptool.core.context.WPContext;
 
 import cbai.util.db.define.TableBean;
 import cbai.util.morphem.MorphemHelper;
-import cbai.util.sqlconvert.NormalSqlConverter;
 import cbai.util.sqlconvert.SqlConverterAbstract;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class WPGenerateContext extends WPContext {
     private MorphemHelper morphemHelper;
 
@@ -26,10 +28,9 @@ public class WPGenerateContext extends WPContext {
     public WPGenerateContext(WPContext context) {
         super(context.getTableSearchService(), context.getMessageLoaderService(), context.getSessionItemLoaderService());
         List<TableBean> tableList = context.getTableSearchService().listAll();
-        this.sqlConverter = new NormalSqlConverter(tableList);
         List<String[]> extendDic = new ArrayList<>();
         tableList.forEach(table -> {
-            extendDic.add(new String[] { table.getTableName(), table.getTableName() });
+            extendDic.add(new String[] { table.getTableFullName(), table.getTableName() });
             table.getFieldList().forEach(column -> {
                 extendDic.add(new String[] { column.getFieldFullName(), column.getFieldName() });
             });
@@ -38,5 +39,23 @@ public class WPGenerateContext extends WPContext {
         List<String[]> extDicts = MorphemHelper.readExtendDic("classpath:com/neusoft/bsdl/wptool/generate/EXT_DICT.txt");
         extendDic.addAll(extDicts);
         this.morphemHelper = new MorphemHelper(extendDic);
+
+        this.sqlConverter = new SqlConverterAbstract(tableList) {
+
+            @Override
+            protected String customTableAliase(String tableAliase, String tableFullName) {
+                if (!tableAliase.matches("\\w+")) {
+                    String newTableAliase = morphemHelper.getRomaFromKanji(tableAliase);
+                    return "T_" + newTableAliase;
+                }
+                return super.customTableAliase(tableAliase, tableFullName);
+            }
+
+            @Override
+            public Map<String, String> prepareLines(List<String> arg0) {
+                return null;
+            }
+        };
+
     }
 }

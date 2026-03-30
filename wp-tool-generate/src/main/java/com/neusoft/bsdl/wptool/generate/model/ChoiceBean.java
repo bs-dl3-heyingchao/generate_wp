675553c9b879a9616aca87cbf0f54f67e9b1ede9
baichen <baich@neusoft.com>
2026-03-30 20:24:21 +0900

    检索项目S_对应 选择列表名称复数对应

--- wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/model/ChoiceBean.java
+++ wp-tool-generate/src/main/java/com/neusoft/bsdl/wptool/generate/model/ChoiceBean.java
@@ -10,8 +10,11 @@ public class ChoiceBean {
 	public String valueDmItemCode;
 	public String nameDmItemCode1;
 	public String nameDmItemCode2;
+	public String nameDmItemCode3;
 	public String sortDmItemCode1;
 	public String sortDmItemCode2;
+	public String sortDmItemCode3;
 	public String sortType1;
 	public String sortType2;
+	public String sortType3;
 }
